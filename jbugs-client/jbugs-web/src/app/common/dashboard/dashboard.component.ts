5638dfa56efa4fd32f34721b9566d4fa17f00ca6
Dan-Crisitan Cosarca <cosarcadan@gmail.com>
2019-08-29 17:38:00 +0300

    Merge branch 'master' of https://github.com/CosarcaDan/Jbugger into Dan
    # Conflicts:
    #	jbugs-client/jbugs-web/src/app/common/dashboard/dashboard.component.ts
    #	jbugs-client/jbugs-web/src/app/core/services/auth/auth.service.ts
    #	jbugs-client/jbugs-web/src/app/feature/bugs-management/add-bug/add-bug.component.html
    #	jbugs-client/jbugs-web/src/app/feature/bugs-management/bugs-list/bugs-list.component.ts
    
    Bug Notifications "BUG_UPDATED" and "BUG_STATUS_UPDATED" Formated for Front End

--- jbugs-client/jbugs-web/src/app/common/dashboard/dashboard.component.ts
+++ jbugs-client/jbugs-web/src/app/common/dashboard/dashboard.component.ts
@@ -146,5 +146,49 @@ export class DashboardComponent implements OnInit {
         this.languageService.getText('assignedTo') +' : '+bug.assigned+'</div><div>'+'</div>';
       return res;
     }
+    if(notification.type == "BUG_UPDATED")
+    {
+      let bugs: Bug[] = JSON.parse(notification.message);
+      let res:string ='<div>'+this.languageService.getText('bugUpdated')+'</div><div>'+
+        this.languageService.getText('oldBugData') + ': <div></div>'+
+        this.languageService.getText('title') + ' : '+bugs[0].title+'</div><div>'+
+        this.languageService.getText('description') +' : '+bugs[0].description.substr(0,20)+' ...</div><div>'+
+        this.languageService.getText('version') +' : '+bugs[0].version+'</div><div>'+
+        this.languageService.getText('fixedVersion') +' : '+bugs[0].fixedVersion+'</div><div>'+
+        this.languageService.getText('severity') +' : '+bugs[0].severity+'</div><div>'+
+        'Status: ' +' : '+bugs[0].status+'</div><div>'+
+        this.languageService.getText('targetDate') +' : '+ formatDate(bugs[0].targetDate,'dd.MM.yyyy','en')+'</div><div>'+
+        this.languageService.getText('createdBy') +' : '+bugs[0].created+'</div><div>'+
+        this.languageService.getText('assignedTo') +' : '+bugs[0].assigned+'</div><div>'+'<div></div><br>'+
+
+        this.languageService.getText('newBugData') + ': <div></div>'+
+        this.languageService.getText('title') + ' : '+bugs[1].title+'</div><div>'+
+        this.languageService.getText('description') +' : '+bugs[1].description.substr(0,20)+' ...</div><div>'+
+        this.languageService.getText('version') +' : '+bugs[1].version+'</div><div>'+
+        this.languageService.getText('fixedVersion') +' : '+bugs[1].fixedVersion+'</div><div>'+
+        this.languageService.getText('severity') +' : '+bugs[1].severity+'</div><div>'+
+        'Status: ' +' : '+bugs[1].status+'</div><div>'+
+        this.languageService.getText('targetDate') +' : '+ formatDate(bugs[1].targetDate,'dd.MM.yyyy','en')+'</div><div>'+
+        this.languageService.getText('createdBy') +' : '+bugs[1].created+'</div><div>'+
+        this.languageService.getText('assignedTo') +' : '+bugs[1].assigned+'</div><div>'+'</div>';
+      console.log(res);
+      return res;
+
+    }
+    if(notification.type == "BUG_STATUS_UPDATED")
+    {
+      let bugs: Bug[] = JSON.parse(notification.message);
+      let res:string ='<div>'+this.languageService.getText('bugStatusUpdate')+'</div><div>'+
+        this.languageService.getText('oldBugStatus') + ': <div></div>'+
+        this.languageService.getText('title') + ' : '+bugs[0].title+'</div><div>'+
+        'Status: ' +' : '+bugs[0].status+'</div><div><br>'+
+        this.languageService.getText('newBugStatus') + ': <div></div>'+
+        this.languageService.getText('title') + ' : '+bugs[1].title+'</div><div>'+
+
+        'Status: ' +' : '+bugs[1].status+'</div><div>'+'</div>';
+      console.log(res);
+      return res;
+
+    }
   }
 }
